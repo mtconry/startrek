d73846afe7fdc77ad6ee19b3bea8206f9c515560
Michael Conry <mikeconry@gmail.com>
2019-04-09 00:40:49 -0400

    header.js

--- startrek/src/components/header/header.js
+++ startrek/src/components/header/header.js
@@ -3,11 +3,8 @@ import FadeIn from '../transitions/fade-in';
 
 const Header = () => { return(
     <header className="App-header">
-        <FadeIn in={true} duration={600} length={'30px'} direction={'bottom'}>
-            <h2>UNC Coding Boot Camp Assignment #15</h2>
-        </FadeIn>
         <FadeIn in={true} duration={600} length={'50px'} direction={'bottom'} >
-            <h1>NBA Clicky Game</h1>
+            <h1>Star Trek Clicky Game</h1>
         </FadeIn>
     </header>
 )};
